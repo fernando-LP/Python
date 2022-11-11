e1da2004fa40a3c6ee7aa10c81004c564af38241
Fernando Pavão <70675152+fernando-LP@users.noreply.github.com>
2022-11-11 15:10:06 -0300

    Add files via upload

---
+++ Curso_Python/aula1.py
@@ -0,0 +1,13 @@
+# AQUI COMENTA APENAS UMA LINHA
+
+
+"""
+AQUI CONSIGO
+Escrever Varias
+Coisa
+
+Isso é chamado de Docstrig
+"""
+
+
+print(1+1)
---
+++ Curso_Python/aula2.py
@@ -0,0 +1,6 @@
+#funcao print mostra na tela alguma coisa
+#Funcao sep e a forma de separar
+
+
+print(12, 13, sep="-", end="#\n")
+print(66, 40, sep="--")
